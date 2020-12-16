b08b304bde9ecacba2e24e9339a978698e12ad32
hamdaoui hamza <hhamdaoui31@gmail.com>
2020-12-16 17:00:26 +0100

    created the card mockup. next thing will be getting data from the Api.

---
+++ src/components/Card.vue
@@ -0,0 +1,69 @@
+<template>
+    <div class="repository-card">
+        <img :src="image" alt="User's avatar" class="user-avatar"/>
+        <div class="repository-information">
+            <h3> Repository Name</h3>
+            <p> Repository Discription </p>
+            <div class="bottom-ligne">
+                <chip :Text="chips.value1" />
+                <chip :Text="chips.value1" />
+                <p>Time Interval by Owner Name</p>
+            </div>
+
+        </div>
+    </div>
+</template>
+
+<script>
+
+import Chip from "./Chip";
+
+export default {
+    name: "card", 
+    components:{
+        Chip
+    },
+    data: function (){
+        return {
+            chips: {
+                value1: "ok",
+                value2: "ok1",
+            }, 
+            image: "https://iupac.org/wp-content/uploads/2018/05/default-avatar.png"
+        };
+    },
+    created: function (){
+
+    }, 
+    methods: {
+
+    }, 
+}
+</script>
+
+<style lang="scss" scoped>
+.repository-card{
+    display: flex;
+    flex-direction: row;
+
+    & .user-avatar{
+        border-radius: .2em;
+        width: 7em;
+        height: 7em;
+    }
+
+    & .repository-information{
+        margin-left: 1em;
+        display: flex; 
+        flex-direction: column;
+        text-align: left;
+        line-height: .2em;
+        & .bottom-ligne{
+            display: flex; 
+            flex-direction: row;
+            
+        }
+    }
+
+}
+</style>
---
+++ src/components/Chip.vue
@@ -0,0 +1,27 @@
+
+<template>
+    <div class="chip">
+        <p class="text" > {{Text}} </p>
+    </div>
+</template>
+
+<script>
+export default {
+    name:"Chip", 
+    props:["Text"]
+
+}
+
+</script>
+
+<style lang="scss" scoped>
+.chip{
+    padding: 0em 1em;
+    border-radius: 3em;
+    margin: 0em .3em 0em .0em;
+    background: black;
+    & .text{
+        color: white;
+    }
+}
+</style>
